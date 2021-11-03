f971fbb8c9e204900278f334620c51ed52f4b487
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2021-11-03 10:59:51 +0530

    created Good-bad-cuboid.cpp

---
+++ Good-bad-cuboid.cpp
@@ -0,0 +1,48 @@
+### 2nd year , difficulty-level: cakewalk , prerequisites : simple maths
+
+### problem statement : 
+
+Alice was assigned a task to identify  good and bad cuboids.
+A good cuboid is one which is atleast 50cc in volume and whose height is no longer than 40cm.
+write a program for alice to identify whether the given cuboid is good or not.
+
+For example :-
+input :-
+10 20 12
+
+output :- Yes
+Explanation :- the volume of the given cuboid is 2400 cc and the height of the cuboid is also less than 40cm.
+
+
+input format : input contains three integers a,b,c which are  length breadth and height of the cuboid respectively.
+
+output format : print Yes if the given cuboid is good else print No
+
+constraints: a,b,c <= 100
+
+
+
+### Testcases:-
+
+1. 10 20 41
+output:- No
+
+2. 2 5 3
+output:- No
+
+3. 3 8 38
+output:- Yes
+
+4. 4 12 1
+output:- No
+
+5. 9 11 2
+output:- Yes
+
+
+
+
+
+
+
+
