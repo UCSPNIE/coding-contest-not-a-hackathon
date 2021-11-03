cc72f4a809a46b3629d480859477f35afc4c3cbe
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2021-11-03 11:36:04 +0530

    created classic-fizzBuzz.cpp

---
+++ classic-fizzBuzz.cpp
@@ -0,0 +1,38 @@
+### 2nd year , Difficulty-level: Easy , Prerequisites : basic Programming and logical thinking
+
+// problem statement :-
+
+Arvind and suchi were solving the classical FizzBuzz Problem where in the given range of number from 1 to n if the
+number is divisible by 3 print Fizz , if the number is divisible by 5 print Buzz and if the number is divisible by 
+both 3 and 5 then print FizzBuzz. Now shree came in the room and to test their knowledge of programming he  asked them do a modified FizzBuzz problem instead.
+In this modified FizzBuzz Problem if any digit of the number is 3 print Fizz and  if any digit is 5 then print Buzz.
+Help suchi and arvind writing a program for this problem.
+
+for example : n=15
+output:-
+  1
+  2
+  Fizz
+  4
+  Buzz
+  6
+  7
+  8
+  9
+  10
+  11
+  12
+  Fizz 
+  13
+  14
+  Buzz
+  
+Explanation :- In place of 3 we printed Fizz as this contains a digit 3, in place of 5 we printed Buzz as this contains a digit 5,
+  in place of 13 we printed Fizz because it contains a digit 3 and similary in place of 15 we printed buzz as it contains a digit 5.
+    
+    
+    
+Input format: Input contains a single integer n 
+Ouput Format : In the range of 1 to n print fizz if number contains a digit 3 and print buzz if it contains a digit 5 and if none then print the number itself
+  
+  
