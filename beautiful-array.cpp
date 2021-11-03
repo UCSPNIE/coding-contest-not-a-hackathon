e676ce50fd596ce3a140ec4be851315ebdaa4d42
Shivam jha <77166379+shivamm-jha@users.noreply.github.com>
2021-11-03 13:58:05 +0530

    created beautiful-array.cpp

---
+++ beautiful-array.cpp
@@ -0,0 +1,31 @@
+### 2nd year , Difficulty-level : medium (can be given as 3rd question considering there are four questions)
+
+// problem statement :-
+
+
+Hardin and Tessa were playing with an array. They want to find whether this array is beautiful or not. 
+A beautiful array is one in which all the elements of the array are palindromic numbers. write a prpgram to find whether 
+the given array is beautiful or not.
+
+For example :-
+arr[]={111,222,343,545};
+Output : 1 
+
+Explanation :- this array is beautiful because all the elements of this array are palindromes so printed 1 
+  
+Example 2:-
+arr[]={232,666,123};
+output:- 0
+
+Explanation:-
+arr[2]=123 which is not a palindromic number so we printed 0
+  
+Input format : 
+First line contains an integer n which is the size of the array
+In the second line there are N space separated values of the array A[].
+  
+Output format:
+
+print 1 if the array is beautiful and 0 if it is not beautiful
+
+
